c38edb85a78e1d46f1d2adfc692b19618dbcf434
makoeppel <koeppel.ma@googlemail.com>
2022-08-18 23:57:59 +0200

    first python version of mlp

--- MLP/python/mlp.py
+++ MLP/python/mlp.py
@@ -1,8 +1,9 @@
 import numpy as np
+from sklearn.metrics import roc_auc_score
 
 
 class mlp:
-    def __init__(self, input_size, layers=[10, 10, 10, 1], learning_rate=0.2, epochs=10):
+    def __init__(self, input_size, layers=[3, 1], learning_rate=0.1, epochs=5000):
         
         # dataset attributes
         self.input_size = input_size
@@ -12,9 +13,7 @@ class mlp:
         self.weights = []
         self.biases = []
         self.z = []
-        self.h = []
-        self.gradients_weights = []
-        self.gradients_biases = []
+        self.s = []
 
         # hyper-parameters
         self.layers = layers
@@ -27,91 +26,93 @@ class mlp:
         # lets initialize the weights, bias and gradients
         prev = None
         for layer in self.layers:
-            if prev:
-                self.weights.append(0.001 * np.random.randn(prev, layer))
-                self.gradients_weights.append(np.zeros([prev, layer]))
+            if prev is not None:
+                self.weights.append(np.random.uniform(size=(prev, layer)))
             else:
-                self.weights.append(0.001 * np.random.randn(self.input_size, layer))
-                self.gradients_weights.append(np.zeros([self.input_size, layer]))
-            self.biases.append(np.zeros(layer))
-            self.gradients_biases.append(np.zeros(layer))
+                self.weights.append(np.random.uniform(size=(self.input_size, layer)))
+            self.biases.append(np.random.uniform(size=(1, layer)))
             prev = layer
-        
-        self.nn = []
-        for layer in self.layers:
-            self.nn.append(layer)
 
 
     def fit(self, x, y):
         self._build()
         
+        errors = []
         for epoch in range(self.epochs):
-            print(epoch)
-            self.forward(x, y)
-    
+            
+            error, y_pred = self.forward(x, y)
+            auc = roc_auc_score(y, y_pred)
+            print(f"Epoch {epoch} - Error {error} - AUC {auc}")
+            errors.append(error)
+        return errors
+
+
+    def nn_layer(self, w, x, b):
+        return (x @ w) + b
+
+
+    def predict(self, x):
+        
+        prev = None
+        self.z = []
+        self.s = []
+        for w, b in zip(self.weights, self.biases):
+            if prev is not None:
+                z = self.nn_layer(w, prev, b)
+                s = self.sigmoid(z)
+            else:
+                z = self.nn_layer(w, x, b)
+                s = self.sigmoid(z)
+            self.z.append(z)
+            self.s.append(s)
+            prev = s
+
+        return s
+
+
+    def backwards(self, x, y):
+        
+        deltaPrev = None
+        #for j, s in enumerate(reversed(self.s), start=1):
+        for idx in reversed(range(len(self.s))):
+            # calculate gradients
+            if deltaPrev is None:
+                # start with the last value of self.s
+                delta = (self.s[idx] - y) * self.s[idx] * (1 - self.s[idx])
+                gradients = self.s[idx-1].T @ delta
+            elif idx == 0:
+                # for the end we need to take the input data x
+                delta = (deltaPrev @ self.weights[idx+1].T) * self.s[idx] * (1 - self.s[idx])
+                gradients = x.T @ delta
+            else:
+                delta = (deltaPrev @ self.weights[idx+1].T) * self.s[idx] * (1 - self.s[idx])
+                gradients = self.s[idx-1].T @ delta
+            
+            # update parameters
+            self.weights[idx] -= self.learning_rate * gradients
+            self.biases[idx] -= self.learning_rate * np.sum(delta, axis=0, keepdims=True)
+            
+            deltaPrev = delta
+
     
     def forward(self, x, y, train=True):
         
-        # forward path
-        prev = None
-        self.h = []
-        self.z = []
-        for layer, weight, bias in zip(self.layers, self.weights, self.biases):
-            if prev is not None:
-                z = np.dot(prev, weight) + bias
-            else:
-                z = np.dot(x, weight) + bias
-            h = self.sigmoid(z)
-            print(z, h)
-            self.z.append(z)
-            self.h.append(h)
-            prev = h
-        prob = self.softmax(h)
-        loss = self.binary_cross_entropy_loss(prob, y)
+        s = self.predict(x)
         
         if train:
-            # backwards path
-            prev = None
-            for i in reversed(range(len(self.gradients_weights))):
-                if prev is not None:
-                    error = np.dot(self.weights[i], prev) * self.sigmoid_dev(self.z[i])
-                else:
-                    error = (y - prob) * self.sigmoid_dev(self.z[i])
-                print(self.h[i], error.T)
-                self.gradients_weights[i] = np.dot(error.T, self.h[i])
-                self.gradients_biases[i] = error
-                
-                print(self.gradients_biases[i], self.weights[i])
-                self.weights[i] += self.learning_rate * self.gradients_weights[i]
-                self.biases[i] += self.learning_rate * self.gradients_biases[i]
-                
-                prev = error
+            # compute error
+            error = self.squard_error(s, y)
 
-    def softmax(self, scores):
-        exp = np.exp(scores - np.max(scores, axis=-1, keepdims=True))
-        sum_exp = np.sum(exp, axis=-1, keepdims=True)
-        prob =  exp / sum_exp
-        return prob
+            # update parameters
+            self.backwards(x, y)
+
+        return error, s
+
+
+    def squard_error(self, A, y):
+        return (np.mean(np.power(A - y, 2)))/2
         
-        
+
     def sigmoid(self, x):
-        return 1. / (1. + np.exp(-1. * x))
-    
-    
-    def sigmoid_dev(self, x):
-        sigmoid = self.sigmoid(x)
-        ds = sigmoid * (1-sigmoid)
-        return ds
+        return 1. / (1. + np.exp(-x))
 
-
-    def binary_cross_entropy_loss(self, pred, y):
-        # get data shape
-        m = pred.shape[0]
-
-        cross_entropy = y * np.log(pred)
-        cost = -np.sum(cross_entropy)
-        cost = cost / m
-        # remove single deminsional entries from shape
-        loss = np.squeeze(cost)
-        
-        return loss
--- MLP/python/test.py
+++ MLP/python/test.py
@@ -1,8 +1,40 @@
 from mlp import mlp
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler
 
-x = np.genfromtxt('../../data/breast.csv', delimiter=",")
-y = np.genfromtxt('../../data/breast_y.csv', delimiter=",")
 
-cls = mlp(len(x[0]))
-cls.fit(x, y)
+# get data
+toy = False
+if toy:
+    # XOR
+    x = np.array([  [0, 0, 1, 1],
+                    [0, 1, 0, 1]]).T
+    y = np.array([[0, 1, 1, 0]]).T
+else:
+    x = np.genfromtxt('../../data/breast.csv', delimiter=",")
+    y = np.genfromtxt('../../data/breast_y.csv', delimiter=",")
+    x = MinMaxScaler().fit_transform(x)
+
+# train the model
+if toy:
+    cls = mlp(input_size=len(x[0]), layers=[3, 1], learning_rate=0.1, epochs=5000)
+else:
+    cls = mlp(input_size=len(x[0]), layers=[40, 20, 10, 1], learning_rate=0.0001, epochs=10000)
+errors = cls.fit(x, y.reshape(-1,1))
+
+# evaluation
+y_pred = cls.predict(x)
+y_pred = np.where(y_pred >= 0.5, 1, 0)
+num_correct_predictions = (y_pred == y).sum()
+accuracy = (num_correct_predictions / y.shape[0]) * 100
+print('Multi-layer perceptron accuracy: %.2f%%' % accuracy)
+
+# plot training loss
+plt.plot(errors, label="train")
+plt.xlabel("epoch")
+plt.ylabel("MSE")
+plt.legend()
+plt.savefig("python-train-mse.pdf")
+plt.savefig("python-train-mse.png")
